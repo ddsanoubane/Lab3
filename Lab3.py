ccd324dc80f7a5bacb3457e6e85851f28cce200d
ddsanoubane <151870714+ddsanoubane@users.noreply.github.com>
2024-02-11 23:56:09 -0500

    Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Description of Function: A function that will write a text file to your PC with your name.
+
+import os
+
+def text_file():
+    name = input('Enter your name: ')
+    file = name + '.txt'
+
+    f = open(file, 'w')
+    f.close
+
+text_file()
